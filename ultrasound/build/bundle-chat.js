3f3c2d21a55ed4f69e7659f54a50f95a3c291384
Razvan Caliman <razvan.caliman@gmail.com>
2015-09-15 23:24:32 +0100

    add simple sonic chat demo

--- ultrasound/build/bundle-chat.js
+++ ultrasound/build/bundle-chat.js
@@ -477,79 +477,32 @@ var SonicSocket = require('./lib/sonic-socket.js');
 var SonicServer = require('./lib/sonic-server.js');
 var SonicCoder = require('./lib/sonic-coder.js');
 
+var opt_coder = new SonicCoder({
+  freqMin: 440,
+  freqMax: 1760
+});
 
-var form = document.querySelector('[data-symbol-container]')
+var sonicServer = new SonicServer({coder: opt_coder});
+var sonicSocket = new SonicSocket({coder: opt_coder, charDuration: 0.2});
 
-form.addEventListener('change', function(e, value){
-  value = form.elements.symbol.value;
+var form = document.querySelector('[data-message-form]');
+var output = document.querySelector('[data-sonic-chat-output]');
 
-  var index = EMOTICONS.indexOf(value);
-  sonicSocket.send(index.toString());
+form.addEventListener('submit', function(e){
+  e.preventDefault();
+  sonicSocket.send(e.target.elements.msg.value);
+  output.textContent = '';
 })
 
-var toggle = document.querySelector('[data-frequency-toggle]')
-toggle.addEventListener('change', function(e, value){
-  value = toggle.elements.freq.value;
+sonicServer.start();
+sonicServer.on('message', function(message) {
+  console.log(message);
+  output.textContent = message;
+});
 
-  if (value === 'audible'){
-    var coder = new SonicCoder({
-      freqMin: 440,
-      freqMax: 1760
-    });
-    createSonicNetwork(coder);
-  } else {
-    createSonicNetwork();
-  }
-})
-
-var EMOTICONS = ['symbol--1', 'symbol--2', 'symbol--3', 'symbol--4'];
-// Calculate the alphabet based on the emoticons.
-var ALPHABET = generateAlphabet(EMOTICONS);
-var PLACEHOLDER = 'symbol--pending';
-
-var sonicSocket;
-var sonicServer;
-
-createSonicNetwork();
-
-function createSonicNetwork(opt_coder) {
-  // Stop the sonic server if it is listening.
-  if (sonicServer) {
-    sonicServer.stop();
-  }
-  if (opt_coder) {
-    sonicServer = new SonicServer({coder: opt_coder});
-    sonicSocket = new SonicSocket({coder: opt_coder});
-  } else {
-    sonicServer = new SonicServer({alphabet: ALPHABET, debug: false});
-    sonicSocket = new SonicSocket({alphabet: ALPHABET});
-  }
-
-  sonicServer.start();
-  sonicServer.on('message', onIncomingEmoticon);
-  // sonicServer.setDebug(true);
-}
-
-function generateAlphabet(list) {
-  var alphabet = '';
-  for (var i = 0; i < Math.min(list.length, 9); i++) {
-    alphabet += i.toString();
-  }
-  return alphabet;
-}
-
-function onIncomingEmoticon(message) {
-  console.log('message: ' + message);
-  var index = parseInt(message);
-  // Validate the message -- it has to be a single valid index.
-  var isValid = (!isNaN(index) && 0 <= index && index < EMOTICONS.length);
-  var preview = document.querySelector('[data-symbol-preview]');
-
-  if (isValid) {
-    preview.className = EMOTICONS[index];
-  } else {
-    preview.className = PLACEHOLDER;
-  }
-}
+sonicServer.on('character', function(character) {
+  console.warn(character);
+  output.textContent += character;
+});
 
 },{"./lib/sonic-coder.js":2,"./lib/sonic-server.js":3,"./lib/sonic-socket.js":4}]},{},[5]);
