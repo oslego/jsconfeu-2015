2bd144e1e954a3cbd838548655e850b7dc3635f9
Razvan Caliman <razvan.caliman@gmail.com>
2015-09-19 15:20:33 +0100

    add option for muted videos

--- slides/js/reveal.js
+++ slides/js/reveal.js
@@ -2760,6 +2760,7 @@
 				var backgroundImage = slide.getAttribute( 'data-background-image' ),
 					backgroundVideo = slide.getAttribute( 'data-background-video' ),
 					backgroundVideoLoop = slide.hasAttribute( 'data-background-video-loop' ),
+					backgroundVideoMuted = slide.hasAttribute( 'data-background-video-muted' ),
 					backgroundIframe = slide.getAttribute( 'data-background-iframe' );
 
 				// Images
@@ -2772,6 +2773,10 @@
 
 					if( backgroundVideoLoop ) {
 						video.setAttribute( 'loop', '' );
+					}
+
+					if( backgroundVideoMuted ) {
+						video.muted = true;
 					}
 
 					// Support comma separated lists of video sources
